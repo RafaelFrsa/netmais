87630ab1b197251f51c1df8252d2804bbf91af8b
Rafael Fs <Rafael Fs>
2019-03-21 22:40:12 -0300

    Teste 03

--- netmais/settings.py
+++ netmais/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://netmais.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'netmais.herokuapp.com']
 
 
 # Application definition
